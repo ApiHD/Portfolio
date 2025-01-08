06305d6891f94394d28a06d68382a1ad7667a1ea
ApiHD <melchior.fas@gmail.com>
2025-01-08 02:02:36 +0100

    updating CV

--- js/cv.js
+++ js/cv.js
@@ -1,71 +1,64 @@
-
-
-
-function showImages(){
-    var magnifying_area =  document.querySelector('.zoom__aera');
-    var magnifying_img =  document.querySelector('.zoomimg');
-    const magnifying_area1 =  document.querySelectorAll('.zoom__aera');
-    const magnifying_img1 =  document.querySelectorAll('.zoomimg');
-    const links = document.querySelectorAll('.card__link');
+function showImages() {
+    var magnifying_area = document.querySelector('.zoom__aera');
+    var magnifying_img = document.querySelector('.zoomimg');
     const zoomlinks = document.querySelectorAll('.zoom__link');
     const zoompart = document.querySelectorAll('.zoomed__part');
-    const modals = document.querySelectorAll('.modal');
-    const modalimage = document.querySelectorAll('.modal_image');
     const boutons = document.querySelectorAll('.exit__link');
-    const showimg=document.querySelector('.zoomed__part');
-
-
+    const showimg = document.querySelector('.zoomed__part');
 
     zoomlinks.forEach(elem => {
-        elem.addEventListener('mousedown',(event) => {
+        elem.addEventListener('mousedown', (event) => {
             event.preventDefault();
-    
+
             showimg.classList.add('show');
-                
-            magnifying_area.addEventListener("mousemove",moveMagnifying)
-            
+            magnifying_area.addEventListener("mousemove", moveMagnifying);
+
+            // Trigger the zoom effect immediately on mousedown
+            triggerMagnifying(event);
         });
     });
 
+    magnifying_area.addEventListener("mouseup", closeMagnifying);
 
-    //new part 
-
-
-
-    magnifying_area.addEventListener("mouseup",closeMagnifying);
-
-    function closeMagnifying(event){
+    function closeMagnifying(event) {
         magnifying_img.style.transform = 'translate(-50%,-50%) scale(2)';
         zoompart.forEach(zoom => {
-
             zoom.classList.remove('show');
-            magnifying_img.classList.remoove('show')
-        }); 
-    }
-    
-    function moveMagnifying(event){
-
-        magnifying_img.classList.add('show')
-        clientX = event.clientX - magnifying_area.offsetLeft
-        clientY = event.clientY - magnifying_area.offsetTop
-         
-        var mWidth = magnifying_area.offsetWidth
-        var mHeight = magnifying_area.offsetHeight
-        clientX = clientX / mWidth * 100
-        clientY = clientY / mHeight * 100
-    
-        //magnifying_img.style.transform = 'translate(-50%,-50%) scale(2)'
-        magnifying_img.style.transform = 'translate(-'+clientX+'%, -'+clientY+'%) scale(2)'
+            magnifying_img.classList.remove('show');
+        });
     }
 
+    function moveMagnifying(event) {
+        magnifying_img.classList.add('show');
+        calculateMagnifying(event);
+    }
+
+    function triggerMagnifying(event) {
+        // Trigger the same behavior as moveMagnifying on mousedown
+        calculateMagnifying(event);
+        magnifying_img.classList.add('show');
+    }
+
+    function calculateMagnifying(event) {
+        const clientX = event.clientX - magnifying_area.offsetLeft;
+        const clientY = event.clientY - magnifying_area.offsetTop;
+
+        const mWidth = magnifying_area.offsetWidth;
+        const mHeight = magnifying_area.offsetHeight;
+
+        const xPercent = (clientX / mWidth) * 100;
+        const yPercent = (clientY / mHeight) * 100;
+
+        magnifying_img.style.transform = `translate(-${xPercent}%, -${yPercent}%) scale(2)`;
+    }
 
     boutons.forEach(exite => {
-        exite.addEventListener('click',(event) => {
+        exite.addEventListener('click', (event) => {
             event.preventDefault();
             hideModals();
             hideModalsback();
         });
-    })
+    });
 }
 
 showImages();
